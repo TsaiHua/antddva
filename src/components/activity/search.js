3349cbddafa83780f6051f7ce224c1f1a74671a3
hjb <hejinbo@qq.com>
2017-02-25 15:34:02 +0800

    搜索框

--- src/components/activity/search.js
+++ src/components/activity/search.js
@@ -1,57 +1,57 @@
 // 引入 React，组件
-import React,{ PropTypes } from 'react';
+import React, { PropTypes } from 'react'
+
 // 引入阿里的antd视觉组件
 import { Form, Button, Row, Col } from 'antd'
 // 引入布局样式
 import styles from './activity.less';
+// 引入搜索框
+import SearchGroup from '../../ui/search'
 
 // 方法
-const Search = ({
+const search = ({
   field,
   keyword,
-  //onSearch,
+  onSearch,
   onAdd,
-  // form: {
-  //   getFieldDecorator,
-  //   validateFields,
-  //   getFieldsValue
-  // }
+  form: {
+    getFieldDecorator,
+    validateFields,
+    getFieldsValue
+  }
 }) => {
-
-  // const searchGroupProps = {
-  //   field,
-  //   keyword,
-  //   size: 'large',
-  //   select: true,
-  //   selectOptions: [{ value: 'name', name: '姓名' }, { value: 'address', name: '地址' }],
-  //   selectProps: {
-  //     defaultValue: field || 'name'
-  //   },
-  //   onSearch: (value) => {
-  //     onSearch(value)
-  //   }
-  // }
+  const searchGroupProps = {
+    field,
+    keyword,
+    size: 'large',
+    select: true,
+    selectOptions: [{ value: 'name', name: '姓名' }, { value: 'address', name: '地址' }],
+    selectProps: {
+      defaultValue: field || 'name'
+    },
+    onSearch: (value) => {
+      onSearch(value)
+    }
+  }
 
   return (
     <Row gutter={24}>
-     <Col lg={8} md={12} sm={16} xs={24} style={{marginBottom: 16}}>
-       {/* <SearchGroup {...searchGroupProps} /> */}
-     </Col>
-     <Col lg={{offset: 8, span: 8}} md={12} sm={8} xs={24} style={{marginBottom: 16, textAlign: 'right'}}>
-       <Button size='large' type='ghost' onClick={onAdd}>新增活动</Button>
-     </Col>
-   </Row>
-  );
-};
+      <Col lg={8} md={12} sm={16} xs={24} style={{marginBottom: 16}}>
+        <SearchGroup {...searchGroupProps} />
+      </Col>
+      <Col lg={{offset: 8, span: 8}} md={12} sm={8} xs={24} style={{marginBottom: 16, textAlign: 'right'}}>
+        <Button size='large' className={styles.cols} type='ghost' onClick={onAdd}>新增活动</Button>
+      </Col>
+    </Row>
+  )
+}
 
-// 参数验证
-Search.propTypes = {
-    form: PropTypes.object.isRequired,
-    onSearch: PropTypes.func,
-    onAdd: PropTypes.func,
-    field: PropTypes.string,
-    keyword: PropTypes.string
-};
+search.propTypes = {
+  form: PropTypes.object.isRequired,
+  onSearch: PropTypes.func,
+  onAdd: PropTypes.func,
+  field: PropTypes.string,
+  keyword: PropTypes.string
+}
 
-// 暴露方法
-export default Search;
+export default Form.create()(search)
